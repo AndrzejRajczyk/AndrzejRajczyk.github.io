46d82e67d3b43b39b9f5510d360e881d923dcef2
AndrzejRajczyk <rajczyk.andrzej@gmail.com>
2022-06-10 12:44:46 +0200

    1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://https://dashboard.heroku.com/apps/kodilla-task-ar/v1/tasks';
+  var apiRoot = 'https://kodilla-task-ar.herokuapp.com//v1/tasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
